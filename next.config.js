21d1c5d41f76e38d6c9221652130993d85e8e828
luthebao <luthebao97@gmail.com>
2024-05-03 18:56:39 +0700

    update

--- next.config.js
+++ next.config.js
@@ -60,7 +60,6 @@ const securityHeaders = [
 module.exports = () => {
   const plugins = [withContentlayer, withBundleAnalyzer]
   return plugins.reduce((acc, next) => next(acc), {
-    disDir: 'build',
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     eslint: {
