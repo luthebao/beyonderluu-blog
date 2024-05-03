5bc933f6ba9bac5aca3a977390d1e9b0f78722f8
luthebao <luthebao97@gmail.com>
2024-05-03 18:17:01 +0700

    1st upload

---
+++ data/projectsData.ts
@@ -0,0 +1,19 @@
+interface Project {
+    title: string,
+    description: string,
+    href?: string,
+    imgSrc?: string,
+}
+
+const projectsData: Project[] = [
+    // {
+    //   title: 'The Time Machine',
+    //   summary: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    //   to the desired date and press "Go". No more worrying about lost keys or
+    //   forgotten headphones with this simple yet affordable solution.`,
+    //   imgSrc: '/static/images/time-machine.jpg',
+    //   href: '/blog/the-time-machine',
+    // },
+]
+
+export default projectsData
