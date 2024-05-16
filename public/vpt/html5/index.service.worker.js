d10726b43e43ac59dfcbeff75c2aed0987f23ad6
luthebao <luthebao97@gmail.com>
2024-05-16 22:41:23 +0700

    update: demo game

--- public/vpt/html5/index.service.worker.js
+++ public/vpt/html5/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1715504959|305470112';
+const CACHE_VERSION = '1715874040|683394611';
 /** @type {string} */
 const CACHE_PREFIX = 'Magic Campus-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
