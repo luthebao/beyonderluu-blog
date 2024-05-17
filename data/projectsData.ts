56a9c1f6fb0cda456e43e0406f9ffbea6cc39db3
luthebao <luthebao97@gmail.com>
2024-05-17 12:57:39 +0700

    update projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,14 +6,12 @@ interface Project {
 }
 
 const projectsData: Project[] = [
-    // {
-    //   title: 'The Time Machine',
-    //   summary: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    //   to the desired date and press "Go". No more worrying about lost keys or
-    //   forgotten headphones with this simple yet affordable solution.`,
-    //   imgSrc: '/static/images/time-machine.jpg',
-    //   href: '/blog/the-time-machine',
-    // },
+    {
+        title: 'Magic Campus cross-device',
+        description: `Remake Magic Campus without using flash, playable on Mobile/PC/Web.`,
+        imgSrc: '/blog/mc/3/2.png',
+        href: '/blog/mc-mobile-dev-log',
+    },
 ]
 
 export default projectsData
